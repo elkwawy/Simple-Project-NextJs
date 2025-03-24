b7dc237f6368c79c8f8e4150cb30f115693c55d4
unknown <ama991969@gmail.com>
2025-03-24 12:21:20 +0200

    Fixed API fetching issues and improved performance

--- app/not-found.js
+++ app/not-found.js
@@ -6,9 +6,11 @@ export default function NotFound() {
       <h1 className="text-9xl font-bold text-blue-500">404</h1>
       <h2 className="text-3xl font-semibold mt-4">Page Not Found</h2>
       <p className="text-gray-400 mt-2 text-center">
-        Oops! It looks like you've taken a wrong turn. Don't worry, you can go
-        back to the homepage.
+        {
+          "Oops! It looks like you've taken a wrong turn. Don't worry, you can go back to the homepage."
+        }
       </p>
+
       <Link
         href="/"
         className="mt-6 px-6 py-3 bg-blue-600 rounded-lg text-lg font-medium hover:bg-blue-700 transition"
